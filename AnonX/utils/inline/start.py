a9250c84f851964044e1b8f60d21e3f6fb0676df
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2023-04-28 04:10:20 -0700

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -9,8 +9,8 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="𝑨𝒅𝒅 𝑾𝒚𝒏𝒌  𝑴𝒖𝒔𝒊𝒄 𝑰𝒏 𝑮𝒓𝒐𝒖𝒑 ",
-                url=f"https://t.me/Wynk_Music_TetrisBot?startgroup=true",
+                text="𝐀ᑯᑯ 𝐉𝗂ⱺ 𝐒αα𝗏𐓣 𝚰𐓣 𝐆𝗋ⱺυρ ",
+                url=f"https://t.me/Jiosaavn_TetrisBot?startgroup=true",
             )
         ],
         [
@@ -30,8 +30,8 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="𝑨𝒅𝒅 𝑾𝒚𝒏𝒌  𝑴𝒖𝒔𝒊𝒄 𝑰𝒏 𝑮𝒓𝒐𝒖𝒑 ",
-                url=f"https://t.me/Wynk_Music_TetrisBot?startgroup=true",
+                text="𝐀ᑯᑯ 𝐉𝗂ⱺ 𝐒αα𝗏𐓣 𝚰𐓣 𝐆𝗋ⱺυρ",
+                url=f"https://t.me/Jiosaavn_TetrisBot?startgroup=true",
             )
         ],
         [
@@ -39,7 +39,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                 text="𝑯𝒆𝒍𝒑", callback_data="settings_back_helper"
             ),
             InlineKeyboardButton(
-                text="𝑾𝒚𝒏𝒌", url=f"https://wynk.in/music"
+                text="𝑾𝒚𝒏𝒌", url=f"https://www.jiosaavn.com/"
             )
         ],
      ]
