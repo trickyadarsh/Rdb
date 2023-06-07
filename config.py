acb3c6baf91c91e65f6397deddda94b09a019238
trickyadarsh <121372273+trickyadarsh@users.noreply.github.com>
2023-06-07 08:14:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "5844064152").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/DarkAarush/new")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/trickyadarsh/Rdb")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
